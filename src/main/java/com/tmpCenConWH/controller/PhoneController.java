d3cca08d190ae60d6ebaa85bd898afd5fa1364cf
handongqingxue <1417140290@qq.com>
2024-03-04 14:06:20 +0800

    1

---
+++ src/main/java/com/tmpCenConWH/controller/PhoneController.java
@@ -0,0 +1,40 @@
+package com.tmpCenConWH.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.tmpCenConWH.entity.*;
+import com.tmpCenConWH.service.*;
+
+@Controller
+@RequestMapping(PhoneController.MODULE_NAME)
+public class PhoneController {
+
+	public static final String MODULE_NAME="/main";
+	
+	@RequestMapping(value="/login")
+	@ResponseBody
+	public Map<String, Object> login(String username, String password) {
+		
+		System.out.println("username==="+username);
+		System.out.println("password==="+password);
+		
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		
+		if("admin".equals(username)&&"123456".equals(password)) {
+			jsonMap.put("status", "ok");
+		}
+		else {
+			jsonMap.put("status", "no");
+			jsonMap.put("info", "暂无信息");
+		}
+		
+		return jsonMap;
+	}
+}
